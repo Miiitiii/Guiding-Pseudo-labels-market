30ac072fd9efebb9e3c8f56e96fb7d51d1a015d1
Miiitiii <mahdifiruzbakht23@gmail.com>
2025-03-30 15:17:02 +0330

    again changed

--- train_source_market.sh
+++ train_source_market.sh
@@ -1,2 +1,2 @@
 DATA_ROOT=${1:-'data'}
-python train_source.py --data_dir ${DATA_ROOT} --dataset market/source --run_name market_source --num_class 751 --lr 0.001 --batch_size 128 --wandb
+python train_source.py --data_dir ${DATA_ROOT} --dataset market/source --run_name market_source --num_class 751 --lr 0.001 --batch_size 128
